b011481178dfa820bbd4ed6b6704d4f15ed6fb1a
Qiaozhi Bao <qiaozhibao96@gmail.com>
2026-01-11 20:06:49 -0500

    initialize

---
+++ src/test/java/com/imooc/passbook/merchants/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.passbook.merchants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
